1cef9e77c40ac7357f28bab26cffae033e7f59e3
venkateshd <venkateshd@proarch.com>
2017-09-12 11:14:08 +0530

    Resolve nuget issues

--- WebApi/Global.asax.cs
+++ WebApi/Global.asax.cs
@@ -11,7 +11,7 @@ namespace WorkExpress.WebApi
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
         }
     }
 }
