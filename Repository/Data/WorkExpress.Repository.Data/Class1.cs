47c082d115debbe79923bbf9ab0526466fcc8d7c
Akhil Naidu <venkatesh.d542@gmail.com>
2017-09-12 09:42:26 +0530

    Initial architecture for workexpress project

---
+++ Repository/Data/WorkExpress.Repository.Data/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkExpress.Repository.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ WebApi/WorkExpress.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WorkExpress.WebApi.WebApiApplication" Language="C#" %>
